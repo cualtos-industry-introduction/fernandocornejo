cdbdcfce468bb092b32b53e99138715f992737dd
fernandocornejo <45342348+fernandocornejo@users.noreply.github.com>
2020-07-03 16:17:47 -0500

    Add files via upload

---
+++ experiments.py
@@ -0,0 +1,19 @@
+def opciones():
+    op = input("Ingresa una opción: ")
+    if op == "b":
+        print("Seleccionaste b")
+    elif op == "c":
+        print("Seleccionaste c")
+    elif op in ['a', 'e', 'i', 'o', 'u']:
+        print("Seleccionaste una vocal")
+    else:
+        print("Todo lo demás")
+
+if __name__ == "__main__":
+    variable = input("Hey boy: ")
+    print(variable)
+    print([x for x in range(10)])
+    salir = "no"
+    while(salir in ["no", "No", "NO"]):
+        opciones()
+        salir = input("¿Deseas salir? ")
